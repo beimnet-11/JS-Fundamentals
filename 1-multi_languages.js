eaee1fb811291374d848e8e8fc5385cacdf97881
rojaz <beimnet.mi1pa@gmail.com>
2025-10-27 13:36:41 +0300

    update: task 1

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -2,4 +2,6 @@ const note1 = 'C is fun'
 const note2 = 'Python is cool'
 const note3 = 'Javascript is amazing'
 
-console.log(note1 + '\n' + note2 + '\n' + note3)
+console.log(note1)
+console.log(note2)
+console.log(note3)
