710c31782cf71e20672760f2f7003318285cecae
rojaz <beimnet.mi1pa@gmail.com>
2025-10-27 15:48:05 +0300

    task 6: done

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,4 +1,4 @@
-const notes = ['C is fun', 'Python is cool', 'Javascript is amazing']
+const notes = ['C is fun', 'Python is cool', 'JavaScript is amazing']
 
 for (let i=0; i < notes.length; i++) {
     console.log(notes[i])
